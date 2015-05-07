1579b533bfb24d9dcd4793b81f70f73d8f29d815
User <User@H27-NotePC02>
2015-05-07 17:51:17 +0900

    やり直しです

--- src/try_github/Main.java
+++ src/try_github/Main.java
@@ -3,13 +3,13 @@ package try_github;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 		Add add=new Add();
 		Sub sub=new Sub();
-
+		
 		System.out.println(add.add(199, 123));
-
-		System.out.println(sub.sub(199,123));
+		
+		System.out.println(Sub.sub(199,123));
 	}
 
 }
